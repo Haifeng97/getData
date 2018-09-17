70a37c0e147a33c6f5e0dffc5af3c77520f1fc5a
Haifeng97 <zzzxxcvvbbb@163.com>
2018-09-18 03:03:06 +0800

    2

--- getData/test2.py
+++ getData/test2.py
@@ -1,9 +1,11 @@
 import urllib.parse
 import urllib.request
 
-req = urllib.request.Request('http://detail.zol.com.cn/cell_phone_index/subcate57_0_list_1_0_1_2_0_1.html')
-user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
-req.add_header("user-agent", user_agent)
-response_result = urllib.request.urlopen('http://detail.zol.com.cn/cell_phone_index/subcate57_0_list_1_0_1_2_0_1.html').read()
-html = response_result.decode('gbk')
-print(type(html))
+# req = urllib.request.Request('http://detail.zol.com.cn/cell_phone_index/subcate57_0_list_1_0_1_2_0_1.html')
+# user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+# req.add_header("user-agent", user_agent)
+# response_result = urllib.request.urlopen('http://detail.zol.com.cn/cell_phone_index/subcate57_0_list_1_0_1_2_0_1.html').read()
+# html = response_result.decode('gbk')
+# print(type(html))
+a = [1,2,3,4,5,6]
+print(a[1:-3])
